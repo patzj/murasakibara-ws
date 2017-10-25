72063f671778b1634d376902f0dfaf3a693a76f5
Pat Bautista <patzj006@gmail.com>
2017-10-25 16:11:12 +0800

    Blocked sites API

--- src/controllers/blocked-site-controller.js
+++ src/controllers/blocked-site-controller.js
@@ -1,4 +1,9 @@
 import BlockedSite from '../models/blocked-site';
+import responseUtil from '../utils/response-util';
+
+const serverErr = responseUtil.serverErr();
+const notFound = responseUtil.notFound();
+const unprocessable = responseUtil.unprocessable();
 
 export const getAll = (req, res) => {
     BlockedSite.find().exec()
@@ -11,17 +16,75 @@ export const getAll = (req, res) => {
 };
 
 export const getOne = (req, res) => {
-
+    BlockedSite.findOne({url: req.params.url}).exec()
+        .then(blockSite => {
+            if(blockSite) {
+                res.json({
+                    url: blockSite.url,
+                });
+            } else {
+                res.status(notFound.status).json(notFound.text);
+            }
+        })
+        .catch(err => {
+            res.status(serverErr.status).json(serverErr.text);
+        });
 };
 
 export const create = (req, res) => {
+    const data = req.body;
+    if(typeof(data.url) === 'undefined') {
+        res.status(unprocessable.status)
+            .json(unprocessable.text);
+    } else {
+        const blockSite = BlockedSite({
+            url: data.url
+        });
 
+        blockSite.save()
+            .then(() => {
+                res.status(201)
+                    .json({
+                        url: blockSite.url
+                    });
+            })
+            .catch(err => {
+                res.status(serverErr.status).json(serverErr.text);
+            });
+    }
 };
 
 export const update = (req, res) => {
-
+    const data = req.body;
+    BlockedSite.findOneAndUpdate(
+        {url: req.params.url},
+        data,
+        {new: true})
+        .exec()
+        .then(blockSite => {
+            if(blockSite) {
+                res.json({
+                    url: blockSite.url,
+                });
+            } else {
+                res.status(notFound.status).json(notFound.text);
+            }
+        })
+        .catch(err => {
+            res.status(serverErr.status).json(serverErr.text);
+        });
 };
 
 export const remove = (req, res) => {
-
+    BlockedSite.findOneAndRemove({url: req.params.url}).exec()
+        .then(blockSite => {
+            if(blockSite) {
+                res.status(204).json({});
+            } else {
+                res.status(notFound.status).json(notFound.text);
+            }
+        })
+        .catch(err => {
+            res.status(serverErr.status).json(serverErr.text);
+        });
 };

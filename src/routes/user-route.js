eec2acd6e85454ef2c4fd0e264e088b75c1854dd
Pat Bautista <patzj006@gmail.com>
2017-10-25 15:54:48 +0800

    User API

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,6 +1,16 @@
-import { getAll } from '../controllers/user-controller';
+import { getAll,
+    getOne,
+    create,
+    update,
+    remove } from '../controllers/user-controller';
 
 export default (app) => {
     app.route('/users')
-        .get(getAll);
+        .get(getAll)
+        .post(create);
+
+    app.route('/users/:username')
+        .get(getOne)
+        .patch(update)
+        .delete(remove);
 };
--- src/utils/response-util.js
+++ src/utils/response-util.js
@@ -3,12 +3,18 @@ export default {
         return {
             status: 404,
             text: { message: 'Not found' }
-        }
+        };
+    },
+    unprocessable: () => {
+        return {
+            status: 422,
+            text: { message: 'Unprocessable entity' }
+        };
     },
     serverErr: () => {
         return {
             status: 500,
             text: { message: 'Something went wrong' }
-        }
+        };
     }
 };
